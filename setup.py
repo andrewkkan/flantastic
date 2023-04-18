baa7ba8d1d766e5b48aba4296a90d92d3d13457f
Andrew K Kan <andrew.k.kan.civ@us.navy.mil>
2023-04-18 11:08:05 -0700

    Enforce python >= 3.9 in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flantastic',
+    python_requires='>=3.9',
     version='1.0.0',
     url='https://github.com/andrewkkan/flantastic',
     description='Mixture of datasets intended for multi-task training, by combining pre-packaged or custom packaged datasets with prompt templates into a single dataset.',
